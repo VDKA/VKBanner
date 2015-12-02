9314ad1f35f6af62b6b5c8ad91c9cffd7c27688d
Ethan Jackwitz <ethanjackwitz@gmail.com>
2015-12-02 11:00:59 +1000

    Initial Commit

---
+++ VKBanner/VKBanner.h
@@ -0,0 +1,19 @@
+//
+//  VKBanner.h
+//  VKBanner
+//
+//  Created by Ethan Jackwitz on 2/12/2015.
+//  Copyright © 2015 Ethan Jackwitz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VKBanner.
+FOUNDATION_EXPORT double VKBannerVersionNumber;
+
+//! Project version string for VKBanner.
+FOUNDATION_EXPORT const unsigned char VKBannerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VKBanner/PublicHeader.h>
+
+
